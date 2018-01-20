14522ab7303dabbfbd9c05514fa3c29e1c8c5640
Toufiq <burningraven06@gmail.com>
2018-01-20 14:41:14 +0600

    bring product template id into model and show in xml

--- models/models.py
+++ models/models.py
@@ -1,1 +1,9 @@
 from odoo import api, models, fields
+
+class InventoryReports(models.Model):
+   _inherit='stock.quant'
+   product_template_id = fields.Many2one(related='product_id.product_tmpl_id')
+   
+   # def get_product_template_id(self):
+   #    for rec in self:
+   #       self.product_template_id = self.product_id.product_tmpl_id
