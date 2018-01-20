d9587ef182800655c06e97c85673b84b5b3793ac
Toufiq <burningraven06@gmail.com>
2018-01-20 17:41:57 +0600

    send first record as context and display in view

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,7 +5,9 @@ class InventoryReport(http.Controller):
    def index(self, **kw):
       stock_quant = http.request.env['stock.quant']
       current_stock = stock_quant.search([])
+      first_record = current_stock[0]
       context = {
+      	'first_record': first_record,
          'current_stock': current_stock
       }
       return http.request.render('ilyn_inven_report_v2.show_current_stock', context)
