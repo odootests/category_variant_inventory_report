eb7d188c2d905a860310aaa3f3e6f7a40d28fa05
Toufiq <burningraven06@gmail.com>
2018-01-22 16:12:38 +0600

    push code, fix error

--- models/models.py
+++ models/models.py
@@ -1,8 +1,7 @@
 from odoo import api, models, fields, http
 
 class InventoryReports(models.Model):
-	_inherit='stock.quant'
-	name='stock.quant.cust'
+	_inherit='stock.quant'	
 	product_template_id = fields.Integer(compute='get_product_template_id', store=True)
 
 	@api.one
