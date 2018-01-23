85dbda61ea27631028f98b6f5478d3fa18699500
Toufiq <burningraven06@gmail.com>
2018-01-23 10:35:30 +0600

    push code

--- controllers/xlsx_with_api.py
+++ controllers/xlsx_with_api.py
@@ -12,52 +12,57 @@ odoo_api = xmlrpclib.ServerProxy('%s/xmlrpc/2/object' %host)
 
 records = odoo_api.execute_kw(database, user_id, password, 'stock.quant', 'search_read', [[]], {'fields':['product_template_id', 'product_template_id_name', 'product_attribute_id','product_attribute_id_name', 'product_category_id', 'product_category_id_name', 'product_attribute_value_id', 'product_attribute_value_id_name', 'qty'] })
 
-workbook = xlsxwriter.Workbook('MyXLS.xlsx')
+workbook = xlsxwriter.Workbook('Current Inventory.xlsx')
 worksheet = workbook.add_worksheet()
 
 # Formatting variant data
-prodVariantRecords = [];
-
-new_array = [];
-for record in (records):
-	new_array = [record['product_template_id'], record['product_template_id_name'], record['product_attribute_id_name'], record['product_attribute_value_id_name'], record['qty']]
-	prodVariantRecords.append(record['product_category_id_name'])
-
-print prodVariantRecords
-
-# Formatting expected data
-expectedOutput = [];
+prodIdRecords = []
 
 for record in (records):
-	new_array = [record['product_category_id_name'], record['product_template_id_name'], record['product_attribute_id_name'], record['product_attribute_value_id_name'], record['qty']]
-	expectedOutput.append(new_array)
+	if not record['product_template_id'] in prodIdRecords:
+		prodIdRecords.append(record['product_template_id'])
+
+# Formatting expected data 
+expectedOutput = []
+
+for prodId in (prodIdRecords):
+	tempArray = []
+	i = 0
+	for record in (records):	
+		if prodId == record['product_template_id']:
+			if i == 0:
+				tempArray.append(record['product_category_id_name'])
+				tempArray.append(record['product_template_id_name'])
+				i+=1
+			tempArray.append(record['product_attribute_id_name'])
+			tempArray.append(record['product_attribute_value_id_name'])
+			tempArray.append(record['qty'])
+	expectedOutput.append(tempArray)
 
 print expectedOutput
 
-# Start from the first cell. Rows and columns are zero indexed.
-
-# Initialize column name 
-worksheet.write(0, 0, 'Category')
-worksheet.write(0, 1, 'Product')
-worksheet.write(0, 2, 'Variant')
-worksheet.write(0, 3, 'Attribute')
-worksheet.write(0, 4, 'Qty')
+# Write data in file
 
 row = 1
 col = 0
 
+worksheet.write(0, 0, 'Category')
+worksheet.write(0, 1, 'Product')
+
+inc = 2
+for i in range(0,9):
+	worksheet.write(0, inc, 'Variant')
+	worksheet.write(0, inc + 1, 'Attribute')
+	worksheet.write(0, inc + 2, 'Qty')
+	inc += 3
+
 # Iterate over the data and write it out row by row.
 for item in (expectedOutput):
-	worksheet.write(row, col, item[0])
-	worksheet.write(row, col + 1, item[1])
-	worksheet.write(row, col + 2, item[2])
-	worksheet.write(row, col + 3, item[3])
-	worksheet.write(row, col + 4, item[4])
+	counter = 0
+	for i in range(0,len(item)):
+		worksheet.write(row, counter, item[counter])
+		counter += 1
 	row += 1
 
-# Write a total using a formula.
-# worksheet.write(row, 0, 'Total')
-# worksheet.write(row, 1, '=SUM(B1:B5)')
-
 workbook.close()
-print("Success")
+print("Done")
